bb447864731d4e231332c3ca779938d3859a5ed6
shabajunior <58442383+shabajunior@users.noreply.github.com>
2021-12-29 16:57:28 +0300

    Create Float.cs

---
+++ 9/Float.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Single i = 2.7182818284590452f;
+            //используем безопасный кастинг float в double
+            Double a = i;
+            //используем безопасный кастинг float в decimal
+            Decimal c = (Decimal)i; 
+            Console.WriteLine(c);
+            Console.WriteLine(a);
+        }
+    }
+}
